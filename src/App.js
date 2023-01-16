d45142be7ead01c2aaa7307d52500130383ea4a8
Nouman Khan <noumankhan@logicworms.com>
2023-01-16 12:25:38 +0500

    Basic structure
    basic structure of app,and add a form from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <form className="basic-form">
+        <div className="mb-3">
+          <input type="text" className="form-control" placeholder="First Name" />
+        </div>
+        <div className="mb-3">
+          <input type="text" className="form-control" placeholder="Last Name"/>
+        </div>
+        <div className="mb-3">
+          <input type="email" className="form-control" placeholder="Email"/>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
